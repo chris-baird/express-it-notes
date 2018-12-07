d185535a93c0e0632b68b491a306482a95b61293
Chris Baird <chrisbaird05@icloud.com>
2018-12-06 19:57:40 -0800

    idk

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,6 +1,17 @@
 const data = {
     appName: 'Express it todos',
-    todos: ['Do something', 'Do something Pt 2']
+    todos: ['Do something', 'Do something Pt 2'],
+    addTodo: function(todo){
+        this.todos.push(todo);
+    },
+    removeTodo: function(todo){
+        this.todos.forEach((currentTodo,index) => {
+            if(currentTodo === todo){
+                console.log('delete hit');
+                this.todos.splice(index,1);
+            }
+        });
+    }
 }
 
 module.exports = data;
--- routes/todo.js
+++ routes/todo.js
@@ -4,14 +4,17 @@ const data = require('../controllers/todoController');
 
 // Todo routes
 router.get('/', (req, res) => {
-    console.log('todo routes hit');
     res.render('todos', { 'data': data });
-})
+});
 
 router.post('/new', (req, res) => {
-    console.log(req.body.todo);
-    data.todos.push(req.body.todo);
+    data.addTodo(req.body.todo);
     res.render('todos', { 'data': data });
-})
+});
+
+router.delete('/delete/:todo', (req, res) => {
+    data.removeTodo(req.params.todo);
+    res.render('todos', { 'data': data });
+});
 
 module.exports = router;
