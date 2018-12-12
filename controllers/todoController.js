8428548e0d5d83a07d6ead5a0559eda68c040b18
Chris Baird <chrisbaird05@icloud.com>
2018-12-11 21:42:34 -0800

    Added create update and delete

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,77 +1,87 @@
-const Todo = require('../models/todo');
+const Todo = require("../models/todo");
 
-const data = {
-  appName: "Express it todos",
-  todos: ["Do something", "Do something Pt 2"],
-  addTodo: function(todo) {
-    this.todos.push(todo);
-  },
-  removeTodo: function(todo) {
-    this.todos.forEach((currentTodo, index) => {
-      if (currentTodo === todo) {
-        console.log("delete hit");
-        this.todos.splice(index, 1);
-      }
-    });
-  },
-  updateTodo: function(todo, updateTodo) {
-    this.todos.forEach((currentTodo, index) => {
-      if (currentTodo === todo) {
-        console.log("update hit");
-        console.log(todo);
-        console.log(currentTodo);
-        this.todos[index] = updateTodo;
-        console.log(currentTodo);
-      }
-    });
-  }
-};
+// const data = {
+//   appName: "Express it todos",
+//   todos: ["Do something", "Do something Pt 2"],
+//   addTodo: function(todo) {
+//     this.todos.push(todo);
+//   },
+//   removeTodo: function(todo) {
+//     this.todos.forEach((currentTodo, index) => {
+//       if (currentTodo === todo) {
+//         console.log("delete hit");
+//         this.todos.splice(index, 1);
+//       }
+//     });
+//   },
+//   updateTodo: function(todo, updateTodo) {
+//     this.todos.forEach((currentTodo, index) => {
+//       if (currentTodo === todo) {
+//         console.log("update hit");
+//         console.log(todo);
+//         console.log(currentTodo);
+//         this.todos[index] = updateTodo;
+//         console.log(currentTodo);
+//       }
+//     });
+//   }
+// };
 
 const todoController = {
-  appName: "Express it todos",
-  todos: ["Do something", "Do something Pt 2"],
-  addTodo: function(todo) {
-    this.todos.push(todo);
+  // appName: "Express it todos",
+  // todos: ["Do something", "Do something Pt 2"],
+  // addTodo: function(todo) {
+  //   this.todos.push(todo);
+  // },
+  // removeTodo: function(todo) {
+  //   this.todos.forEach((currentTodo, index) => {
+  //     if (currentTodo === todo) {
+  //       console.log("delete hit");
+  //       this.todos.splice(index, 1);
+  //     }
+  //   });
+  // },
+  // updateTodo: function(todo, updateTodo) {
+  //   this.todos.forEach((currentTodo, index) => {
+  //     if (currentTodo === todo) {
+  //       console.log("update hit");
+  //       console.log(todo);
+  //       console.log(currentTodo);
+  //       this.todos[index] = updateTodo;
+  //       console.log(currentTodo);
+  //     }
+  //   });
+  // },
+  getAllTodos: (req, res) => {
+    Todo.find({})
+      .then(todo => {
+        res.render("todos", { data: todo });
+      })
+      .catch(err => res.status(400).json(err));
   },
-  removeTodo: function(todo) {
-    this.todos.forEach((currentTodo, index) => {
-      if (currentTodo === todo) {
-        console.log("delete hit");
-        this.todos.splice(index, 1);
-      }
-    });
-  },
-  updateTodo: function(todo, updateTodo) {
-    this.todos.forEach((currentTodo, index) => {
-      if (currentTodo === todo) {
-        console.log("update hit");
-        console.log(todo);
-        console.log(currentTodo);
-        this.todos[index] = updateTodo;
-        console.log(currentTodo);
-      }
-    });
-  },
-  getAllTodos: function(req, res) {
-    console.log('hit get all todo routes');
-    let data = ["Do something", "Do something Pt 2"];
-
-    // let tempData = {
-    //   name: 'Chris',
-    //   todo: 'Finnish Homework'
-    // }
-    // Todo.create(tempData).then(todo => {
-    //   console.log(todo);
-    // })
-
-    Todo.find({}).then(todo => {
+  addTodo: (req, res) => {
+    const todo = req.body;
+    Todo.create(todo).then(todo => {
       console.log(todo);
-      res.render('todos', { data: todo});
+      res.redirect('/todos');
     }).catch(err => res.status(400).json(err));
-    
+  },
+  updateTodo: (req, res) => {
+    const todoId = req.params.todo;
+    const updateTodo = req.body;
+    console.log(todoId);
+    console.log(updateTodo);
+    Todo.findByIdAndUpdate({ _id:todoId }, updateTodo).then(updateTodo => {
+      res.redirect('/todos');
+    }).catch(err => res.status(400).json(err));
+  },
+  removeTodo: (req, res) => {
+    const todoId = req.params.todo;
+    console.log(todoId);
+    Todo.findByIdAndRemove(todoId).then(deletedTodo => {
+      res.redirect('/todos');
+    }).catch(err => res.status(400).json(err));
   }
 };
-
-
 
 module.exports = todoController;
