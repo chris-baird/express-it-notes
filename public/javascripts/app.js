55780f6c38fdb920f512f4ce54ff703d0b755743
Chris Baird <chrisbaird05@icloud.com>
2018-12-15 14:14:22 -0800

    Stuff

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,62 +1,65 @@
-console.log('Hey custom js is loaded');
-console.log('page loaded');
+console.log("Hey custom js is loaded");
+console.log("page loaded");
 let action;
-    $("button").on("click", event => {
-      let eventTarget = event.target.id;
-      
-      if(eventTarget === 'new-todo'){
-        console.log('yes');
-        let dataTarget = event.target.attributes['data-action'].value;
-        console.log(dataTarget);
-        // action = 
-      }
+$("button").on("click", event => {
+  let eventTarget = event.target.id;
 
-      if(eventTarget === "submit") {
-        const todoName = $('.name-form').val();
-        const todoBody = $('.todo-form').val();
-        $.ajax({
-          url: '/todos/',
-          type: 'POST',
-          data: {
-            name: todoName,
-            todo: todoBody
-          },
-          success: function(result) {
-            window.location.reload();
-            $('.name-form').val('');
-            $('.todo-form').val('');
-          }
-        })
-      }
+  if (eventTarget === "new-todo") {
+    console.log("yes");
+    action = event.target.attributes["data-action"].value;
+    console.log(action);
+  } else if (eventTarget === "update-todo") {
+    action = event.target.attributes["data-action"].value;
+    console.log("yes");
+    console.log(action);
+  }
 
-      if (eventTarget === "delete") {
-        const todo = event.target.attributes["data-todo"].value;
-        $.ajax({
-          url: "/todos/" + todo,
-          type: "DELETE",
-          success: function(result) {
-            window.location.reload();
-          }
-        });
-      }
-
-      if (eventTarget === "update") {
-        $('#exampleModal').modal('show');
-        const todo = event.target.attributes["data-todo"].value;
-        const todoName = $('.name-form').val();
-        const todoBody = $('.todo-form').val();
-        if(todoName && todoBody){
-        $.ajax({
-          url: "/todos/" + todo,
-          type: "PUT",
-          data: {
-            name: todoName,
-            todo: todoBody
-          },
-          success: function(result) {
-            window.location.reload();
-          }
-        });
-      }
+  if (eventTarget === "submit") {
+    const todoName = $(".name-form").val();
+    const todoBody = $(".todo-form").val();
+    $.ajax({
+      url: "/todos/",
+      type: "POST",
+      data: {
+        name: todoName,
+        todo: todoBody
+      },
+      success: function(result) {
+        window.location.reload();
+        $(".name-form").val("");
+        $(".todo-form").val("");
       }
     });
+  }
+
+  if (eventTarget === "delete") {
+    const todo = event.target.attributes["data-todo"].value;
+    $.ajax({
+      url: "/todos/" + todo,
+      type: "DELETE",
+      success: function(result) {
+        window.location.reload();
+      }
+    });
+  }
+
+  if (eventTarget === "update-todo") {
+    $("#exampleModal").modal("show");
+    const todo = event.target.attributes["data-todo"].value;
+    const todoName = $(".name-form").val();
+    const todoBody = $(".todo-form").val();
+    if (todoName && todoBody) {
+      $.ajax({
+        url: "/todos/" + todo,
+        type: "PUT",
+        data: {
+          name: todoName,
+          todo: todoBody
+        },
+        success: function(result) {
+          window.location.reload();
+        }
+      });
+    }
+  }
+});
