21923b9d9b10f134a695842ee8c72d8b075c6d97
Chris Baird <chrisbaird05@icloud.com>
2018-12-17 18:45:01 -0800

    got modal working

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,36 +4,75 @@ let modalInfo = {
   modalNameInput: $('.name-form'),
   modalTodoInput: $('.todo-form'),
   modalSubmit: $('#submit-todo'),
+  modalTodoId:''
 }
-// const displayModal = function(action) {
-//   if (action === "add-todo") {
-//     modalInfo.modalTitle.text('Add New Todo');
-//     $("#exampleModal").modal("show");
-//   } else if (action === "update-todo") {
-//     modalInfo.modalTitle.text('Update Todo');
-//     $("#exampleModal").modal("show");
-//   }
-// };
+
 $("button").on("click", event => {
   let eventTarget = event.target.id;
-
+  let eventAction;
   if (eventTarget === "new-todo" || eventTarget === "update-todo") {
     action = event.target.attributes["data-action"].value;
-    // displayModal(action);
     if (action === "add-todo") {
+      eventAction = event.target.attributes[4].value;
+      console.log(eventAction);
       modalInfo.modalTitle.text('Add New Todo');
       modalInfo.modalNameInput.val('');
       modalInfo.modalTodoInput.val('');
       modalInfo.modalSubmit.text('Add Todo');
+      modalInfo.modalSubmit.attr('data-action', 'new');
       $("#exampleModal").modal("show");
     } else if (action === "update-todo") {
+      console.log(event.target.attributes[1].value);
+      modalInfo.modalTodoId = event.target.attributes[1].value
+      console.log(modalInfo.modalTodoId);
+      eventAction = event.target.attributes[2].value;
+      console.log(eventAction);
       modalInfo.modalTitle.text('Update Todo');
       let todoName = event.target.attributes[3].value;
       let todoBody = event.target.attributes[4].value;
       modalInfo.modalNameInput.val(todoName);
       modalInfo.modalTodoInput.val(todoBody);
       modalInfo.modalSubmit.text('Update Todo');
+      modalInfo.modalSubmit.attr('data-action', 'update');
       $("#exampleModal").modal("show");
+    }
+  }
+
+  if($(`#${eventTarget}`).attr('data-action') === 'new'){
+    console.log('new hit');
+    const todoName = $(".name-form").val();
+    const todoBody = $(".todo-form").val();
+    $.ajax({
+      url: "/todos/",
+      type: "POST",
+      data: {
+        name: todoName,
+        todo: todoBody
+      },
+      success: function(result) {
+        window.location.reload();
+        $(".name-form").val("");
+        $(".todo-form").val("");
+      }
+    });
+  } else if($(`#${eventTarget}`).attr('data-action') === 'update'){
+    console.log('update hit');
+    // const todo = event.target.attributes["data-todo"].value;
+    console.log(event.target.attributes);
+    const todoName = $(".name-form").val();
+    const todoBody = $(".todo-form").val();
+    if (todoName && todoBody) {
+      $.ajax({
+        url: "/todos/" + modalInfo.modalTodoId,
+        type: "PUT",
+        data: {
+          name: todoName,
+          todo: todoBody
+        },
+        success: function(result) {
+          window.location.reload();
+        }
+      });
     }
   }
 
@@ -50,23 +89,23 @@ $("button").on("click", event => {
   // }
 
   // Create todo
-  if (eventTarget === "submit-todo") {
-    const todoName = $(".name-form").val();
-    const todoBody = $(".todo-form").val();
-    $.ajax({
-      url: "/todos/",
-      type: "POST",
-      data: {
-        name: todoName,
-        todo: todoBody
-      },
-      success: function(result) {
-        window.location.reload();
-        $(".name-form").val("");
-        $(".todo-form").val("");
-      }
-    });
-  }
+  // if (eventTarget === "submit-todo") {
+  //   const todoName = $(".name-form").val();
+  //   const todoBody = $(".todo-form").val();
+  //   $.ajax({
+  //     url: "/todos/",
+  //     type: "POST",
+  //     data: {
+  //       name: todoName,
+  //       todo: todoBody
+  //     },
+  //     success: function(result) {
+  //       window.location.reload();
+  //       $(".name-form").val("");
+  //       $(".todo-form").val("");
+  //     }
+  //   });
+  // }
 
   // Delete todo
   if (eventTarget === "delete") {
