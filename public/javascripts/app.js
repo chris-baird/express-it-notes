11bd832da2daebbc2305ec20b88c1bd156bfa491
Chris Baird <chrisbaird05@icloud.com>
2018-12-17 17:26:21 -0800

    Working on modal logic

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,20 +1,53 @@
-console.log("Hey custom js is loaded");
-console.log("page loaded");
 let action;
+let modalInfo = {
+  modalTitle: $('.modal-title'),
+  modalNameInput: $('.name-form'),
+  modalTodoInput: $('.todo-form'),
+  modalSubmit: $('#submit-todo'),
+}
+// const displayModal = function(action) {
+//   if (action === "add-todo") {
+//     modalInfo.modalTitle.text('Add New Todo');
+//     $("#exampleModal").modal("show");
+//   } else if (action === "update-todo") {
+//     modalInfo.modalTitle.text('Update Todo');
+//     $("#exampleModal").modal("show");
+//   }
+// };
 $("button").on("click", event => {
   let eventTarget = event.target.id;
 
-  if (eventTarget === "new-todo") {
-    console.log("yes");
+  if (eventTarget === "new-todo" || eventTarget === "update-todo") {
     action = event.target.attributes["data-action"].value;
-    $("#exampleModal").modal("show");
-    console.log(action);
-  } else if (eventTarget === "update-todo") {
-    action = event.target.attributes["data-action"].value;
-    $("#exampleModal").modal("show");
-    console.log("yes");
-    console.log(action);
+    // displayModal(action);
+    if (action === "add-todo") {
+      modalInfo.modalTitle.text('Add New Todo');
+      modalInfo.modalNameInput.val('');
+      modalInfo.modalTodoInput.val('');
+      modalInfo.modalSubmit.text('Add Todo');
+      $("#exampleModal").modal("show");
+    } else if (action === "update-todo") {
+      modalInfo.modalTitle.text('Update Todo');
+      let todoName = event.target.attributes[3].value;
+      let todoBody = event.target.attributes[4].value;
+      modalInfo.modalNameInput.val(todoName);
+      modalInfo.modalTodoInput.val(todoBody);
+      modalInfo.modalSubmit.text('Update Todo');
+      $("#exampleModal").modal("show");
+    }
   }
+
+  // if (eventTarget === "new-todo") {
+  //   console.log("yes");
+  //   action = event.target.attributes["data-action"].value;
+  //   $("#exampleModal").modal("show");
+  //   console.log(action);
+  // } else if (eventTarget === "update-todo") {
+  //   action = event.target.attributes["data-action"].value;
+  //   $("#exampleModal").modal("show");
+  //   console.log("yes");
+  //   console.log(action);
+  // }
 
   // Create todo
   if (eventTarget === "submit-todo") {
